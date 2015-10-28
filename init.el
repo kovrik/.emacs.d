1725589bdb63c417d156489983d946d5334d456c
kovrik <kovrik0@gmail.com>
2015-10-28 20:44:45 +1300

    diminish rainbow-mode

--- init.el
+++ init.el
@@ -170,7 +170,7 @@
 (use-package command-log-mode :defer t)
 (use-package restclient :defer t)
 (use-package iedit :defer t)
-(use-package rainbow-mode :defer t)
+(use-package rainbow-mode :defer t :diminish rainbow-mode)
 (use-package focus :defer t)
 (use-package color-theme :defer t :config (color-theme-initialize))
 (use-package fixme-mode :config (fixme-mode t))
