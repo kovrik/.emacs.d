1f47b2759b24d2b7e018e4f0c529965218a52251
kovrik <kovrik0@gmail.com>
2022-10-28 08:24:06 +1300

    Lispy: disable C- bindings.

--- init.el
+++ init.el
@@ -669,6 +669,7 @@
     (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
 
   (use-package lispy
+    :init (setq lispy-key-theme '(special lispy))
     :ensure t
     :config
     (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
